05bf6ca235e028fbd33bf0787589ecfc23ccdef0
Sergey Matveev <s.matveev@rian.ru>
2024-08-06 16:56:08 +0300

    Kadane algo

--- Algorithms/Algorithms/main.swift
+++ Algorithms/Algorithms/main.swift
@@ -7,5 +7,15 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Start Algo")
+
+print("Kadane max sum1: \(SolutionKadane().maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))")
+print("Kadane max sum2: \(SolutionKadane().maxSubArray([1]))")
+print("Kadane max sum3: \(SolutionKadane().maxSubArray([5,4,-1,7,8]))")
+
+print("Kadane max sum1: \(SolutionDefault().maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))")
+print("Kadane max sum2: \(SolutionDefault().maxSubArray([1]))")
+print("Kadane max sum3: \(SolutionDefault().maxSubArray([5,4,-1,7,8]))")
+
+
 
